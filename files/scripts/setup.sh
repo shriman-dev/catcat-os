60e76c77238d6b00b3365b0f11ac8a2689593e20
shriman <shriman-w@proton.me>
2024-10-09 19:40:12 +0530

    Wed Oct 09 19:40:12 2024

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -9,7 +9,7 @@ sed -i 's/"pip3", //g' /usr/share/ublue-os/topgrade.toml || true
 #fix librewolf/firefox delayed launch issue
 #'/^hosts:/ s/mdns4_minimal/myhostname &/'
 sed  -i '/^hosts:/ s/myhostname//; /^hosts:.*files\s\+myhostname/! s/mdns4_minimal/myhostname &/' /etc/nsswitch.conf
-
+ 
 
 systemctl enable fstrim.timer nix.mount \
                  everyFewMins.service everyFewMins.timer catcat-system-setup.service
