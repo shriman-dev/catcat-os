f0278d40d9d85ed157dfa1542a1daa876b29cf3b
shriman <shriman-w@proton.me>
2024-10-09 19:18:33 +0530

    Wed Oct 09 19:18:33 2024

---
+++ files/scripts/cleanup.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+
+chmod 000 /usr/bin/ibus
+chmod 000 /usr/bin/ibus-daemon
+chmod 000 /usr/bin/ibus-setup
+chmod 000 /usr/libexec/evolution-source-registry
+chmod 000 /usr/libexec/evolution-addressbook-factory
+chmod 000 /usr/libexec/evolution-calendar-factory
+chmod 000 /usr/libexec/evolution-data-server/evolution-alarm-notify
+
+
+systemctl disable brew-dir-fix.service brew-setup.service brew-update.service \
+                  brew-upgrade.service
+
+
+rm -rf /home/linuxbrew
+rm -rf /usr/share/ublue-os/homebrew
+rm -vf /usr/lib/systemd/system/brew-dir-fix.service
+rm -vf /usr/lib/systemd/system/brew-setup.service
+rm -vf /usr/lib/systemd/system/brew-update.service
+rm -vf /usr/lib/systemd/system/brew-upgrade.service
+
+systemctl disable input-remapper.service NetworkManager-wait-online.service \
+                  systemd-networkd-wait-online.service tailscaled.service \
+                  setroubleshootd.service
+
+systemctl  disable sshd.service
+systemctl --global mask sshd.service
+
+systemctl --global mask tracker-miner-fs-3.servicee \
+                        tracker-miner-fs-control-3.service \
+                        tracker-miner-rss-3.service tracker-writeback-3.service \
+                        tracker-xdg-portal-3.service
+
+rm -vf /usr/lib/systemd/user/tracker-miner-fs-3.service
+rm -vf /usr/lib/systemd/user/tracker-miner-fs-control-3.service
+rm -vf /usr/lib/systemd/user/tracker-miner-rss-3.service
+rm -vf /usr/lib/systemd/user/tracker-writeback-3.service
+rm -vf /usr/lib/systemd/user/tracker-xdg-portal-3.service
+
+rm -vf /etc/xdg/autostart/ibus-mozc-launch-xwayland.desktop
+rm -vf /etc/xdg/autostart/nvidia-settings-load.desktop
+rm -vf /etc/xdg/autostart/org.gnome.Software.desktop
+rm -vf /etc/xdg/autostart/tracker-miner-fs-3.desktop
+rm -vf /etc/xdg/autostart/tracker-miner-rss-3.desktop
+rm -rvf /etc/skel/*
+
+rm -vf /usr/share/fish/vendor_conf.d/nano-default-editor.fish
+rm -vf /usr/share/fish/vendor_conf.d/bazzite-neofetch.fish
+rm -vf /usr/share/fish/functions/fish_greeting.fish
+rm -vf /usr/share/applications/gnome-ssh-askpass.desktop
+rm -vf /usr/libexec/topgrade/mozilla-gnome-theme-update
+
---
+++ files/scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+sed -i '/^PRETTY_NAME/s/Bazzite/CatCat/' /usr/lib/os-release
+sed -i "s|.*issue_discards =.*|issue_discards = 1|"  /etc/lvm/lvm.conf
+sed -i 's/"pip3", //g' /usr/share/ublue-os/topgrade.toml || true
+
+#fix librewolf/firefox delayed launch issue
+#'/^hosts:/ s/mdns4_minimal/myhostname &/'
+sed  -i '/^hosts:/ s/myhostname//; /^hosts:.*files\s\+myhostname/! s/mdns4_minimal/myhostname &/' /etc/nsswitch.conf
+
+
+systemctl enable fstrim.timer nix.mount \
+                 everyFewMins.service everyFewMins.timer catcat-system-setup.service
