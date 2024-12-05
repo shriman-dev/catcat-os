6610cb40f33b1236fd627f04e399452115487141
shriman <shriman-w@proton.me>
2024-12-05 18:13:56 +0530

    Thu Dec 05 18:13:56 2024

--- files/system/etc/profile.d/nix.sh
+++ files/system/etc/profile.d/nix.sh
@@ -1,3 +1,4 @@
+<<'###'
 if ystemctl --quiet is-active nix.mount; then
 
 	# Source global configuration
@@ -19,3 +20,4 @@ if ystemctl --quiet is-active nix.mount; then
 	unset NIX_STATE_HOME
 
 fi
+###
