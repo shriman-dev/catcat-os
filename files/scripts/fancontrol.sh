5a73d9d0b25e89024f818aac622b5cf778089ceb
shriman <shriman-w@proton.me>
2024-12-18 20:48:43 +0530

    Wed Dec 18 20:48:43 2024

--- files/scripts/fancontrol.sh
+++ files/scripts/fancontrol.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -oue pipefail
+pwd
+
 curl -Lo /tmp/MControlCenter.tar.gz https://github.com/dmitry-s93/MControlCenter/releases/latest/download/MControlCenter-0.4.1-bin.tar.gz
 
 mkdir -p /tmp/MControlCenter
 tar -xf /tmp/MControlCenter.tar.gz -C /tmp/MControlCenter --strip-components=1
 
-cd /tmp/MControlCenter
-./install
-cd -
+/tmp/MControlCenter/install
 
