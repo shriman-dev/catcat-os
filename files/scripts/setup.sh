f4a7e617bd4fdcc198e48c927e80682515a13726
shriman <shriman-w@proton.me>
2024-12-31 18:39:40 +0530

    Tue Dec 31 18:39:40 2024

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -4,6 +4,7 @@ set -oue pipefail
 echo -e "\n$0\n"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+$SCRIPT_DIR/cleanup.sh
 
 # copy system files
 FILESDIR="$(dirname $SCRIPT_DIR)"
@@ -13,7 +14,6 @@ cp -drvf ${FILESDIR}/dconf/*  /etc/dconf/db/distro.d/
 cp -drvf ${FILESDIR}/dconf/*  /usr/etc/dconf/db/distro.d/
 cp -drf  ${FILESDIR}/skel     /etc/
 
-$SCRIPT_DIR/cleanup.sh
 $SCRIPT_DIR/pre-setup.sh
 $SCRIPT_DIR/rpm-ostree.sh
 $SCRIPT_DIR/flatpak.sh
