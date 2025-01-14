76c86d4786d2cc930bcb77b371ff61d400d7fe58
shriman <shriman-w@proton.me>
2025-01-14 14:18:12 +0530

    Tue Jan 14 14:18:12 2025

--- files/scripts/fancontrol.sh
+++ files/scripts/fancontrol.sh
@@ -2,7 +2,8 @@
 set -oue pipefail
 echo -e "\n$0\n"
 ###
-curl -Lo /tmp/MControlCenterrrr https://github.com/dmitry-s93/MControlCenter/releases/latest/download/MControlCenter-0.4.1-bin.tar.gz
+
+curl -Lo /tmp/MControlCenterrrr $(curl -s -X GET https://api.github.com/repos/dmitry-s93/MControlCenter/releases/latest | grep -i '"browser_download_url": "[^"]*.tar.gz"' | cut -d '"' -f4)
 
 mkdir -p /tmp/MControlCenter
 tar -xf /tmp/MControlCenterrrr -C /tmp/MControlCenter --strip-components=1
