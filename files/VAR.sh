a616227482aafd68f99ee58ca27e6abefe12e56f
shriman <shriman-w@proton.me>
2025-01-21 17:18:01 +0530

    Tue Jan 21 17:18:01 2025

--- files/VAR.sh
+++ files/VAR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+MAJOR_VERSION=41
+DEFAULT_TAG="latest"
 DATESTAMP="$(date "+%Y%m%d")"
 TIMESTAMP="$(TZ="Asia/Kolkata" date "+%H%M%S")"
 AH_DATE=$(date -u +%Y\-%m\-%d\T%H\:%M\:%S\Z)
-MAJOR_VERSION=41
-DEFAULT_TAG="latest"
