cb577433c16132d423d6555e4230f2b370d9ffbb
shriman <shriman-w@proton.me>
2025-02-15 11:20:58 +0530

    Sat Feb 15 11:20:58 2025

--- files/scripts/security.sh
+++ files/scripts/security.sh
@@ -41,7 +41,7 @@ no-resolv
 bind-interfaces
 addn-hosts=/etc/hosts' > /etc/NetworkManager/dnsmasq.d/00-defaults.conf"
 
-sh -c "curl -sf https://raw.githubusercontent.com/shriman-dev/dns-blocklist/refs/heads/main/dnsmasq.d/blocklist[00-09].conf > /etc/NetworkManager/dnsmasq.d/blocklist.conf" || true
+sh -c "curl -sf https://raw.githubusercontent.com/shriman-dev/dns-blocklist/refs/heads/main/dnsmasq.d/blocklist[00-03].conf > /etc/NetworkManager/dnsmasq.d/blocklist.conf" || true
 
 # get hblock config
 mkdir -p /etc/hblock
