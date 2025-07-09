26c8bbb682c2b96b15caa949c8c32dd6cd9d5e98
shriman <shriman-w@proton.me>
2025-07-09 20:18:48 +0530

    Wed Jul 09 20:18:48 2025

--- files/scripts/systemd.sh
+++ files/scripts/systemd.sh
@@ -5,7 +5,6 @@ echo -e "\n$0\n"
 #sddm.service
 #auto-power-profile.service \
 services_enable() {
-  command -v hhdctl && systemctl -f enable unl0kr-ask-password.service
   systemctl -f enable \
     fstrim.timer \
     nix.mount \
