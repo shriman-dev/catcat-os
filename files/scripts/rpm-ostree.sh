4d4e60256658d2b2264fad7680532cf9437ea8dd
shriman <shriman-w@proton.me>
2025-08-24 13:42:18 +0530

    Sun Aug 24 13:42:18 2025

--- files/scripts/rpm-ostree.sh
+++ files/scripts/rpm-ostree.sh
@@ -133,7 +133,7 @@ add_repos
 
 # debloat
 #ibus-libpinyin ibus-hangul ibus-m17n ibus-mozc ibus-typing-booster
-rpm-ostree override remove topgrade fastfetch plocate gnome-shell-extension-just-perfection gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-compiz-alike-magic-lamp-effect gnome-shell-extension-compiz-windows-effect openssh-askpass nvtop
+dnf5 -y remove topgrade fastfetch plocate gnome-shell-extension-just-perfection gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-compiz-alike-magic-lamp-effect gnome-shell-extension-compiz-windows-effect openssh-askpass nvtop
 #sunshine gnome-browser-connector
 
 security='firejail firewall-config usbguard usbguard-selinux usbguard-notifier pam_mount'
@@ -204,4 +204,4 @@ all_pkgs=(
     "${extras[@]}"
 )
 
-rpm-ostree install antimicrox
+dnf5 -y install ${all_pkgs[@]}
