6e11aae2ba6ba0754f4bab662a028925a266cf85
shriman <shriman-w@proton.me>
2025-10-09 18:59:40 +0530

    Thu Oct 09 18:59:40 2025

--- files/scripts/dconf-dump.sh
+++ files/scripts/dconf-dump.sh
@@ -105,7 +105,7 @@ dconfDump '/org/gnome/mutter/' "/output-luminance/d" $DCONF_DIR/wmpreferences
 # shell
 declare -A shellPathsWithSed=(
     ['/org/gnome/shell/']="
-s|favorite-apps=.*|favorite-apps=['org.gnome.Nautilus.desktop', 'org.gnome.Ptyxis.desktop', 'io.gitlab.librewolf-community.desktop', 'io.freetubeapp.FreeTube.desktop','page.kramo.Cartridges.desktop']|
+s|favorite-apps=.*|favorite-apps=['org.gnome.Nautilus.desktop', 'org.gnome.Ptyxis.desktop', 'io.gitlab.librewolf-community.desktop', 'io.freetubeapp.FreeTube.desktop', 'page.kramo.Cartridges.desktop']|
 /shell\/extensions/,\$d
 /command-history/d
 /enabled-extensions/d
