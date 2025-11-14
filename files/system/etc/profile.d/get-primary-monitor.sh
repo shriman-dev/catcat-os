b694047cc13d940c78cc2c41079b0958c7133c2c
shriman <shriman-w@proton.me>
2025-11-14 14:27:49 +0530

    Fri Nov 14 14:27:49 2025

--- files/system/etc/profile.d/get-primary-monitor.sh
+++ files/system/etc/profile.d/get-primary-monitor.sh
@@ -1,6 +1,6 @@
 CATCAT_CONF="${HOME}/.local/share/catcat-os"
 
 [[ $(id -u) -eq 1000 ]] && {
-    local primary_monitor="$(gdbus call --session --dest org.gnome.Mutter.DisplayConfig --object-path /org/gnome/Mutter/DisplayConfig --method org.gnome.Mutter.DisplayConfig.GetCurrentState | grep -o "\[((.*), \[(" | tr -cd '[:alnum:],' | cut -d "," -f2,4 | tr "," "-")"
+    primary_monitor="$(gdbus call --session --dest org.gnome.Mutter.DisplayConfig --object-path /org/gnome/Mutter/DisplayConfig --method org.gnome.Mutter.DisplayConfig.GetCurrentState | grep -o "\[((.*), \[(" | tr -cd '[:alnum:],' | cut -d "," -f2,4 | tr "," "-")"
     echo "${primary_monitor}" > "${CATCAT_CONF}/primary-monitor"
 }
