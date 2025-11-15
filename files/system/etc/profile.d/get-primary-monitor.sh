b7ba6b1798cf4680b62475dd396f23f6d89ff96b
shriman <shriman-w@proton.me>
2025-11-16 00:03:23 +0530

    Sun Nov 16 00:03:23 2025

--- files/system/etc/profile.d/get-primary-monitor.sh
+++ files/system/etc/profile.d/get-primary-monitor.sh
@@ -1,6 +1,15 @@
-CATCAT_CONF="${HOME}/.local/share/catcat-os"
+# Set primary monitor for dash to panel
+usr_dconf_ext="/usr/etc/dconf/db/distro.d/extensions"
+primary_monitor="$(gdbus call --session --dest org.gnome.Mutter.DisplayConfig --object-path /org/gnome/Mutter/DisplayConfig --method org.gnome.Mutter.DisplayConfig.GetCurrentState | grep -o "\[((.*), \[(" | tr -cd '[:alnum:],' | cut -d "," -f2,4 | tr "," "-")"
 
-[[ $(id -u) -eq 1000 ]] && {
-    primary_monitor="$(gdbus call --session --dest org.gnome.Mutter.DisplayConfig --object-path /org/gnome/Mutter/DisplayConfig --method org.gnome.Mutter.DisplayConfig.GetCurrentState | grep -o "\[((.*), \[(" | tr -cd '[:alnum:],' | cut -d "," -f2,4 | tr "," "-")"
-    echo "${primary_monitor}" > "${CATCAT_CONF}/primary-monitor"
-}
+
+
+if ! dconf read /org/gnome/shell/extensions/dash-to-panel/panel-positions | grep ${primary_monitor}; then
+
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-sizes $(grep -E "^.*panel-sizes=.*$" "${usr_dconf_ext}" | sed "s|panel-sizes='{\".*\":|panel-sizes='{\"${primary_monitor}\":|" | sed "s|panel-sizes=||")
+
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-element-positions $(grep -E "^.*panel-element-positions=.*$" "${usr_dconf_ext}" | sed "s|panel-element-positions='{\".*\":\[{\"|panel-element-positions='{\"${primary_monitor}\":\[{\"|" | sed "s|panel-element-positions=||")
+
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-positions $(grep -E "^.*panel-positions=.*$" "${usr_dconf_ext}" | sed "s|panel-positions='{\".*\":|panel-positions='{\"${primary_monitor}\":|" | sed "s|panel-positions=||")
+
+fi
