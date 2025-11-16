9f5bc70f6d3e920ada183ca7e7b7a940c50df18b
shriman <shriman-w@proton.me>
2025-11-16 14:54:48 +0530

    Sun Nov 16 14:54:48 2025

--- files/system/etc/profile.d/get-primary-monitor.sh
+++ files/system/etc/profile.d/get-primary-monitor.sh
@@ -3,7 +3,7 @@ usr_dconf_ext="/usr/etc/dconf/db/distro.d/extensions"
 primary_monitor="$(gdbus call --session --dest org.gnome.Mutter.DisplayConfig --object-path /org/gnome/Mutter/DisplayConfig --method org.gnome.Mutter.DisplayConfig.GetCurrentState | grep -o "\[((.*), \[(" | tr -cd '[:alnum:],' | cut -d "," -f2,4 | tr "," "-")"
 
 
-if [ -n "$DISPLAY" ]; then
+if [[ -n "${DISPLAY}" ]]; then
 if ! dconf read /org/gnome/shell/extensions/dash-to-panel/panel-positions | grep -q ${primary_monitor}; then
 
 dconf write /org/gnome/shell/extensions/dash-to-panel/panel-sizes $(grep -E "^.*panel-sizes=.*$" "${usr_dconf_ext}" | sed "s|panel-sizes='{\".*\":|panel-sizes='{\"${primary_monitor}\":|" | sed "s|panel-sizes=||")
