188b75fdb3b46eda580766fbe3d084ea0fc7a739
shriman <shriman-w@proton.me>
2025-12-29 13:29:39 +0530

    Mon Dec 29 13:29:39 2025

--- files/system/etc/profile.d/startup.sh
+++ files/system/etc/profile.d/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #/usr/libexec/catcat-user-setup >/dev/null 2>&1
+( /usr/bin/bat cache --build >/dev/null 2>&1 & )
--- files/system/usr/share/fish/vendor_conf.d/envar.fish
+++ files/system/usr/share/fish/vendor_conf.d/envar.fish
@@ -7,18 +7,13 @@ set -gx NIXPKGS_ALLOW_UNFREE 1
 set -gx ni "$HOME/.local/state/nix/profiles/profile/bin"
 
 # Set var for android home
+if test -d /storage/emulated/0
+  set -gx ANHOME "/storage/emulated/0"
+end
 if test -d $HOME/.local/share/waydroid/data/media/0
   set -gx ANHOME "$HOME/.local/share/waydroid/data/media/0"
-else
-  set -gx ANHOME "/storage/emulated/0"
 end
 
 # Set bat command as manpager for syntax highlighting
 set -gx MANROFFOPT "-c"
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-# python ta-lib
-set -gx TA_INCLUDE_PATH "$HOME/.local/state/nix/profiles/profile/include"
-set -gx TA_LIBRARY_PATH "$HOME/.local/state/nix/profiles/profile/lib"
-
-
