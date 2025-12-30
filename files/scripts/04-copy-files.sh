20e23ad04e9cf1ae1468a4e56d3d8c08f8e08c40
shriman <shriman-w@proton.me>
2025-12-30 14:51:08 +0530

    Tue Dec 30 14:51:08 2025

--- files/scripts/04-copy-files.sh
+++ files/scripts/04-copy-files.sh
@@ -8,5 +8,4 @@ cp -vf ${SETUP_DIR}/setup_files/cosign.pub /etc/pki/containers/catcat-os.pub
 FILESDIR="$(dirname ${SETUP_DIR})"
 cp -drf  ${FILESDIR}/system/* /
 cp -arvf ${FILESDIR}/dconf/*  /etc/dconf/db/distro.d/
-cp -drf  ${FILESDIR}/skel     /etc/
 log "INFO" "Copying done."
