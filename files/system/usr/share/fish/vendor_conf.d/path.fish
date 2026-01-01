a572f212839abdce5a0c5b1e47630b6e1a91f7e5
shriman <shriman-w@proton.me>
2026-01-02 00:29:41 +0530

    Fri Jan 02 00:29:41 2026

--- files/system/usr/share/fish/vendor_conf.d/path.fish
+++ files/system/usr/share/fish/vendor_conf.d/path.fish
@@ -1,7 +1,7 @@
 _check_local_config
 
 # PATHS
-set paths_to_add "/usr/games" "$HOME/.bin" "$HOME/.local/bin" "$HOME/.local/sbin" "$HOME/.local/podman/bin" "$HOME/.cargo/binnn" "$HOME/Android/platform-tools" "$HOME/Android/cmdline-tools/bin"
+set paths_to_add "/usr/games" "$HOME/.bin" "$HOME/.local/bin" "$HOME/.local/sbin" "$HOME/.local/podman/bin" "$HOME/.cargo/bin" "$HOME/Android/platform-tools" "$HOME/Android/cmdline-tools/bin"
 
 for path in $paths_to_add
   if not contains -- $path $PATH
