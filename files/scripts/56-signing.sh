5380ab5851e384df3745dbb7e00ac37fc8672282
shriman <shriman-w@proton.me>
2026-02-02 19:42:09 +0530

    Mon Feb 02 19:42:09 2026

--- files/scripts/56-signing.sh
+++ files/scripts/56-signing.sh
@@ -11,7 +11,7 @@ COSIGN_PUB_KEY="/etc/pki/containers/${PROJECT_NAME}.pub"
 POLICY_FILE="/etc/containers/policy.json"
 
 mkdir -vp /etc/pki/containers /etc/containers/registries.d
-cp -vf "${BUILD_SETUP_DIR}/setup_files/cosign.pub" "${COSIGN_PUB_KEY}"
+cp -vf "/ctx/cosign.pub" "${COSIGN_PUB_KEY}"
 
 # TODO: Add secure boot signing in non ublue images
 
@@ -20,11 +20,10 @@ cp -vf "${BUILD_SETUP_DIR}/setup_files/cosign.pub" "${COSIGN_PUB_KEY}"
 [[ "$(jq -r '.default[0].type' "${POLICY_FILE}")" == "insecureAcceptAnything" ]] &&
     cp -v "${TEMPLATE_POLICY}" "${POLICY_FILE}"
 
-jq --arg image_name "${IMAGE_NAME}" \
-   --arg project_registry "${PROJECT_REGISTRY}" \
+jq --arg registry_image_url "${PROJECT_REGISTRY}/${IMAGE_NAME}" \
    --arg cosign_pub_key "${COSIGN_PUB_KEY}" \
    '.transports.docker |=
-    { ($project_registry + "/" +  $image_name): [
+    { $registry_image_url: [
         {
             "type": "sigstoreSigned",
             "keyPath": $cosign_pub_key,
@@ -35,6 +34,8 @@ jq --arg image_name "${IMAGE_NAME}" \
     ] } + .' "${POLICY_FILE}" > "/tmp/POLICY.tmp"
 
 mv -v "/tmp/POLICY.tmp" "${POLICY_FILE}"
+
+cat "${POLICY_FILE}"
 
 echo "docker:
   ${PROJECT_REGISTRY}/${IMAGE_NAME}:
