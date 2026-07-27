464676d346790c68e3aa424697ddfa3690e8070c
shriman <shriman-w@proton.me>
2026-07-27 22:50:35 +0530

    Mon Jul 27 22:50:35 2026

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euox pipefail
 umask 0022
 SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$(dirname ${SCRIPT_DIR})/files/scripts/script_lib/funcvar.sh"
